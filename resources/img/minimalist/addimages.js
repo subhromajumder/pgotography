3b30ea89932aad810510be8babc3cc332ae52715
subhromajumder <subhromajumder19@gmail.com>
2020-06-30 20:23:48 +0530

    final2

--- resources/img/minimalist/addimages.js
+++ resources/img/minimalist/addimages.js
@@ -1,11 +1,11 @@
-const total_images = 4;
+const total_images = 5;
 
 (function () {
   let html, newHtml;
   for (let i = 1; i <= total_images; i++) {
     //create HTML string with placeholder text
     html =
-      '<div class="img"><a href="resources/img/street/#.jpeg" data-toggle="lightbox" data-gallery="example-gallery" class=""><img src="resources/img/street/#.jpeg" class="img-fluid" /></a></div>';
+      '<div class="img"><a href="resources/img/minimalist/#.jpeg" data-toggle="lightbox" data-gallery="example-gallery" class=""><img src="resources/img/minimalist/#.jpeg" class="img-fluid" /></a></div>';
     //replace the placeholder text with some actual data
     newHtml = html.replace(/#/g, i);
     //insert the HTML into the DOM
