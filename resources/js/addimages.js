61b314f96ea474dc633c979cf64f3494624cba55
subhromajumder <subhromajumder19@gmail.com>
2020-06-30 19:50:22 +0530

    final

---
+++ resources/js/addimages.js
@@ -0,0 +1,13 @@
+var total_images = 4;
+(function () {
+  var html, newHtml;
+  for (var i = 1; i <= total_images; i++) {
+    //create HTML string with placeholder text
+    html =
+      '<div class="img"><a href="resources/img/street/#.jpeg" data-toggle="lightbox" data-gallery="example-gallery" class=""><img src="resources/img/street/#.jpeg" class="img-fluid" /></a></div>';
+    //replace the placeholder text with some actual data
+    newHtml = html.replace(/#/g, i);
+    //insert the HTML into the DOM
+    document.querySelector(".gallery").insertAdjacentHTML("beforeend", newHtml);
+  }
+})();
